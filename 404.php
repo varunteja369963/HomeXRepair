f4dbf8917939c96e98825aa89f4a2e883a8b1cfe
Marneni Varun Teja <varunteja369963@gmail.com>
2022-05-23 13:17:41 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>404 Page Not Found - HomeXRepair</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale = 1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+<meta name="title" content="404 Page Not Found - HomeXRepair">
+<meta name="author" content="HomeXRepair">
+<meta name="description" content="HomeXRepair - Call: +91-9347951264 to book technicians only at INR 249 or get quote right now before you book your services.">
+
+<meta name="robots" content="index, follow">
+<meta name="bingbot" content="index, follow">
+<meta name="googlebot" content="index, follow">
+
+<link rel="prefetch" href="https://homexrepair.com/css/homexrepair.min.css" as="style">
+<link rel="prerender" href="https://homexrepair.com/">
+<link rel="canonical" href="https://homexrepair.com/404">
+
+<meta property="og:locale" content="en_US">
+<meta property="og:type" content="website">
+<meta property="og:title" content="404 Page Not Found - HomeXRepair">
+<meta property="og:description" content="HomeXRepair - Call: +91-9347951264 to book technicians only at INR 249 or get quote right now before you book your services.">
+<meta property="og:image" content="https://homexrepair.com/img/logo-homexrepair.png">
+<meta property="og:url" content="https://homexrepair.com/404">
+<meta property="og:site_name" content="HomeXRepair">
+
+<meta property="article:published_time" content="2020-07-22T12:01:00+00:00">
+<meta property="article:modified_time" content="2020-07-22T12:08:00+00:00">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="404 Page Not Found - HomeXRepair">
+<meta name="twitter:description" content="HomeXRepair - Call: +91-9347951264 to book technicians only at INR 249 or get quote right now before you book your services.">
+<meta name="twitter:image" content="https://homexrepair.com/img/logo-homexrepair.png">
+<meta name="twitter:site" content="@homexrepair">
+<meta name="twitter:creator" content="@Sabiduria_in">
+
+<link rel="shortcut icon" href="https://homexrepair.com/img/icon-homexrepair.ico" type="image/x-icon">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://homexrepair.com/js/homexrepair.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://homexrepair.com/css/homexrepair.min.css"> 
+</head>
+<body>
+<div class = "total_page">
+<?php include_once('includes/nav.php'); ?>
+
+<section class = "summary-section">
+<div class = "summary">
+<div class = "summary-info">
+<div class = "summary-info-inner">
+<div class = "heading-div"><h1 class = "heading">Page Not Found 404</h1></div>
+<div class = "summary-matter">
+Please click here to go to <a href = "https://homexrepair.com/">HomeXRepair</a> Home Page
+</div>
+<div class = "call-now-div"><a href = "tel:+918106967414">CALL NOW</a></div>
+</div>
+</div>
+<div class = "get-quote" id = "get-quote"  itemscope itemtype="http://schema.org/Invoice">
+<div class = "query">
+    <div class = "query-label"><p>GET QUOTE RIGHT NOW</p></div>
+    <div class = "problem-with-product">
+      <div class = "select-product">
+        <div class = "select-product-label"><p>Select Your Product<sup>&#40;optional&#41;</sup></p></div>
+        <div class = "select-product-options">
+            <label class="container" itemprop="name">Washing Machine
+                <input type = "radio" id = "wm-radio" name = "prdouct" value = "wm" data-appliance-name = "washing-machine">
+                <span class="checkmark"></span>
+              </label>
+              <label class="container" itemprop="name">Refrigerator
+                <input type = "radio" id = "ref-radio" name = "prdouct" value = "ref" data-appliance-name = "Refrigerator">
+                <span class="checkmark"></span>
+              </label>
+              <label class="container" itemprop="name">MicroWave Oven
+                <input type = "radio" id = "mo-radio" name = "prdouct" value = "mo" data-appliance-name = "microwave-oven">
+                <span class="checkmark"></span>
+              </label>
+              <label class="container" itemprop="name">Air Conditioner(AC)
+                <input type = "radio" id = "ac-radio" name = "prdouct" value = "ac" data-appliance-name = "air-conditioner">
+                <span class="checkmark"></span>
+              </label>
+        </div>
+        <div class = "sub-category" id = "sub-category">
+        <div class = "sub-category-label"><p>Which of these best describe your problem?</p></div>
+            <select name="wm" id = "wm" class="custom-select mb-3">
+                <option selected>Please Select</option>
+                <option>Washing Maching is not working</option>
+                <option>Washing Maching is making loud noise</option>
+                <option>Water overflows</option>
+                <option>Washing Maching door issues</option>
+                <option>Washing Maching stop mid cycle</option>
+                <option>Washing Maching leaking water</option>
+                <option>Washing Maching fills slowly or not filling</option>
+                <option>General Servicing</option>
+                <option>Installation/Uninstallation</option>
+                <option>Other Problem</option>
+            </select>
+
+            <select name="ref" id = "ref" class="custom-select mb-3">
+                <option selected>Please Select</option>
+                <option>Refrigerator not working</option>
+                <option>No/Less cooling</option>
+                <option>Water Leakage issue</option>
+                <option>Gas Leakage/refilling</option>
+                <option>Refrigerator light not working</option>
+                <option>Refrigerator is noisy or loud</option>
+                <option>General Servicing</option>
+                <option>Installation/Un-Installation</option>
+                <option>Other Problem</option>
+            </select>
+
+            <select name="mo" id = "mo" class="custom-select mb-3">
+                <option selected>Please Select</option>   
+                <option>Microwave Oven not working</option>
+                <option>Microwave Oven not heating</option>
+                <option>Buttons & Keypad not working</option>
+                <option>Microwave door won't open</option>
+                <option>Microwave light bulb not working</option>
+                <option>Microwave display not working</option>
+                <option>General Servicing</option>
+                <option>Installation/Un-Installation</option>
+                <option>Other Problem</option>
+            </select>
+
+            <select name="ac" id = "ac" class="custom-select mb-3">
+                <option selected>Please Select</option>
+                <option>Air Conditioner not working</option>
+                <option>No/Less Cooling</option>
+                <option>Over Cooling</option>
+                <option>Leaking Water</option>
+                <option>Air Conditioner fans not working</option>
+                <option>Air Conditioner compressor won't run</option>
+                <option>Air Conditioner is noisy</option>
+                <option>Air Conditioner remote control not working</option>
+                <option>General Servicing</option>
+                <option>Installation/Un-Installation</option>
+                <option>Other Problem</option>
+            </select>
+          </div>
+
+          </div>
+          <div class = "mobile-no">
+            <div class = "mobile-no-label">Mobile Number&#40;&#43;91&#41;<i class = "imp">&nbsp;&#42;</i></div>
+            <div class = "mobile-no-sub-label"><p>&#40;We assure your number is not misused.&#41;</p></div>
+            <input type = "text" class = "mobile-no-box" id = "mobile-no-box" placeholder = "Phone Number"/>
+            <div class = "mobile-no-ack" id = "mobile-no-ack"></div>
+          </div>
+        </div>
+        <div class = "submit-form">
+          <button type = "button" class = "submit-query" id = "submit-query">GET QUOTE<span><i class = "fa fa-arrow-right"></i></span></button>
+        </div>
+    </div>
+</div>
+</div>
+</section>
+
+<section class = "popular-links-section">
+<div class = "popular-links">
+<div class = "popular-links-label"><p>People Who Visited This Page Also Visited</p></div>
+<div class = "popular-links-buttons">
+<button type = "button" id = "most-visited-button">Most Visited</button>
+<button type = "button" id = "near-you-button">Near You</button>
+<button type = "button" id = "brands-button">Brands</button>
+<button type = "button" id = "areas-button">Areas</button>
+</div>
+<div class = "popular-links-a">
+<div class = "most-visited-div" id = "most-visited-div">
+<a href = "">Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Refridgerator Repair</a>&nbsp;&#124;
+<a href = "">Microwave Oven Repair</a>&nbsp;&#124;
+<a href = "">Air Conditioner Repair</a>&nbsp;&#124;
+</div>
+<div class = "near-you-div" id = "near-you-div">
+<a href = "">Washing Machine Repair Near me</a>&nbsp;&#124;
+<a href = "">Refridgerator Repair Near me</a>&nbsp;&#124;
+<a href = "">Microwave Oven Repair Near me</a>&nbsp;&#124;
+<a href = "">Air Conditioner Repair Near me</a>&nbsp;&#124;
+</div>
+<div class = "brands-div" id = "brands-div">
+<a href = "">Lg Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Lg Refrigerator Repair</a>&nbsp;&#124;
+<a href = "">Lg Microwave Oven Repair</a>&nbsp;&#124;
+<a href = "">Lg AC Repair</a>&nbsp;&#124;
+<a href = "">Samsung Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Samsung Refrierator Repair</a>&nbsp;&#124;
+<a href = "">Samsung Microwave Oven Repair</a>&nbsp;&#124;
+<a href = "">Samsung AC Repair</a>&nbsp;&#124;
+<a href = "">Whirlpool Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Whirlpool Refrigerator Repair</a>&nbsp;&#124;
+<a href = "">Whirlpool Microwave Oven Repair</a>&nbsp;&#124;
+<a href = "">Whirlpool AC Repair</a>&nbsp;&#124;
+<a href = "">Ifb Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Ifb Refrigerator Repair</a>&nbsp;&#124;
+<a href = "">Ifb Oven Repair</a>&nbsp;&#124;
+<a href = "">Ifb AC Repair</a>&nbsp;&#124;
+</div>
+<div class = "areas-div" id = "areas-div">
+<a href = "">Washing Machine Repair In Hyderabad</a>&nbsp;&#124;
+<a href = "">Refridgerator Repair In Hyderabad</a>&nbsp;&#124;
+<a href = "">Microwave Oven Repair In Hyderabad</a>&nbsp;&#124;
+<a href = "">Air Conditioner Repair In Hyderabad</a>&nbsp;&#124;
+</div>
+</div>
+</div>
+</section>
+
+<?php include_once("includes/footer.php"); ?>
+<?php include_once("includes/fixed-call-now.php"); ?>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>HomeXRepair - Appliance Repair In Hyderabad</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale = 1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta name="distribution" content="global">
+<meta http-equiv="content-language" content="en-gb">
+<meta name="geo.region" content="IN" />
+
+<meta name="title" content="HomeXRepair - Appliance Repair In Hyderabad">
+<meta name="author" content="HomeXRepair">
+<meta name="description" content="Best Home appliance repair service provider in hyderabad and secunderabad. Book washing machine repair, fridge repair, ac repair, oven repair expert technicians from HomeXRepair.">
+
+<meta name="robots" content="index, follow">
+<meta name="bingbot" content="index, follow">
+<meta name="googlebot" content="index, follow">
+
+<link rel="prefetch" href="https://homexrepair.com/css/homexrepair.min.css" as="style">
+<link rel="shortlink" href = "https://homexrepair.com/">
+<link rel="canonical" href="https://homexrepair.com/">
+
+<meta property="og:locale" content="en_US">
+<meta property="og:type" content="website">
+<meta property="og:title" content="HomeXRepair - Appliance Repair In Hyderabad">
+<meta property="og:description" content="Best Home appliance repair service provider in hyderabad and secunderabad. Book washing machine repair, fridge repair, ac repair, oven repair expert technicians from homexrepair.">
+<meta property="og:image" content="https://homexrepair.com/img/logo-homexrepair.png">
+<meta property="og:url" content="https://homexrepair.com">
+<meta property="og:site_name" content="HomeXRepair">
+
+<meta property="article:published_time" content="2020-07-21T12:01:00+00:00">
+<meta property="article:modified_time" content="2020-07-21T12:08:00+00:00">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="HomeXRepair - Appliance Repair In Hyderabad">
+<meta name="twitter:description" content="Best Home appliance repair service provider in hyderabad and secunderabad. Book washing machine repair, fridge repair, ac repair, oven repair expert technicians from homexrepair.">
+<meta name="twitter:image" content="https://homexrepair.com/img/logo-homexrepair.png">
+<meta name="twitter:site" content="@homexrepair">
+<meta name="twitter:creator" content="@Sabiduria_in">
+
+<link rel="shortcut icon" href="https://homexrepair.com/img/icon-homexrepair.ico" type="image/x-icon">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://homexrepair.com/js/homexrepair.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://homexrepair.com/css/homexrepair.min.css">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-173602327-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-173602327-1');
+</script>
+
+<script>
+/* RECORD USER */
+$(document).ready(function() {
+  if (sessionStorage.session_set == undefined) {
+    sessionStorage.setItem("session_set","true");
+    setTimeout(function() {
+        var path = $(location).attr('href');
+        $.ajax({
+            type: 'POST',
+            url: "https://homexrepair.com/request/recorduser.php", 
+            data: {
+                'path': path,
+                'brand': 'homexrepair',
+                'node': 'home'
+            },
+            success: function(bef_data){
+                var data = $.trim(bef_data);
+            }
+        });
+    }, 4000);
+  }
+});
+/* RECORD USER */
+</script>
+    
+</head>
+<body>
+<div class = "total_page">
+<?php include_once('includes/nav.php'); ?>
+<?php include_once('includes/summary.php'); ?>
+<?php include_once('includes/get-quote.php'); ?>
+<?php include_once('includes/why-to-hire-us.php'); ?>
+<?php include_once('includes/services.php'); ?>
+
+<section class = "about-us-section">
+<div class = "about-us">
+    <div class = "about-us-label"><strong><h1>What HomeXRepair Does?</h1></strong></div>
+<div class = "about-us-matter">
+<p>At present we are only handling appliance repair, but soon we are going to include electrical services, plumbing, carpentry, painting, etc. in our service portal. But as of now, we are only engaged in the home repair services like washing machine repair, fridge repair, ac repair, oven repair.</p>
+<ul>
+    <li>
+<a href = "#"><h2 class = "fw400 fs16">Washing Machine Repair</h2></a>
+</li>
+<li>
+<a href = "#"><h2 class = "fw400 fs16">Fridge Repair</h2></a>
+</li>
+<li>
+<a href = "#"><h2 class = "fw400 fs16">AC Repair</h2></a>
+</li>
+<li>
+<a href = "#"><h2 class = "fw400 fs16">Oven Repair</h2></a>
+</li>
+</ul>
+</div>
+</div>
+
+<div class = "about-us">
+<div class = "about-us-label"><strong><h1>Areas HomeXRepair Operates?</h1></strong></div>
+<div class = "about-us-matter">
+<p>HomeXRepair operates in all areas of Hyderabad and Secunderabad i.e Kukatpally, Hi-Tech City, Banjara Hills, Jubilee Hills, Ameerpet, Dilsukhnagar, Koti, Miyapur, Gachibowli, LB Nagar, Santhosh Nagar, Uppal, Attapur, Tolichowki. And planning to span to the areas of Vishakapatnam, Mumbai, Chennai, Pune, Banglore.</p>
+</div>
+</div>
+
+<div class = "about-us">
+<div class = "about-us-label"><strong><h1>About HomeXRepair Empolyees?</h1></strong></div>
+<div class = "about-us-matter">
+<p>HomeXRepair has more than 50+ employees operating in Hyderabad and Secunderabad. Our employees have more than 15+ years of experience in dealing with the home appliance repair service.</p>
+</div>
+</div>
+
+<div class = "about-us">
+<div class = "about-us-label"><strong><h1>Will HomeXRepair Provide Guarentee For Appliance Repair?</h1></strong></div>
+<div class = "about-us-matter">
+<p>Unlike other service provider. HomeXRepair assures with a 100% guarantee on every service provided. With our trained and qualified technicians you do not get a need for guarantee. But in case if there is a problem with repair or parts, you will get a free repeated repair service within 45 days and 90 days warranty on parts.</p>
+</div>
+</div>
+</section>
+
+<section class = "popular-links-section">
+<div class = "popular-links">
+<div class = "popular-links-label"><p>People Who Visited This Page Also Visited</p></div>
+<div class = "popular-links-buttons">
+<button type = "button" id = "most-visited-button">Most Visited</button>
+<button type = "button" id = "near-you-button">Near You</button>
+<button type = "button" id = "brands-button">Brands</button>
+<button type = "button" id = "areas-button">Areas</button>
+</div>
+<div class = "popular-links-a">
+<div class = "most-visited-div" id = "most-visited-div">
+<a href = "">Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Refridgerator Repair</a>&nbsp;&#124;
+<a href = "">Microwave Oven Repair</a>&nbsp;&#124;
+<a href = "">Air Conditioner Repair</a>&nbsp;&#124;
+</div>
+<div class = "near-you-div" id = "near-you-div">
+<a href = "">Washing Machine Repair Near me</a>&nbsp;&#124;
+<a href = "">Refridgerator Repair Near me</a>&nbsp;&#124;
+<a href = "">Microwave Oven Repair Near me</a>&nbsp;&#124;
+<a href = "">Air Conditioner Repair Near me</a>&nbsp;&#124;
+</div>
+<div class = "brands-div" id = "brands-div">
+<a href = "">Lg Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Lg Refrigerator Repair</a>&nbsp;&#124;
+<a href = "">Lg Microwave Oven Repair</a>&nbsp;&#124;
+<a href = "">Lg AC Repair</a>&nbsp;&#124;
+<a href = "">Samsung Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Samsung Refrierator Repair</a>&nbsp;&#124;
+<a href = "">Samsung Microwave Oven Repair</a>&nbsp;&#124;
+<a href = "">Samsung AC Repair</a>&nbsp;&#124;
+<a href = "">Whirlpool Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Whirlpool Refrigerator Repair</a>&nbsp;&#124;
+<a href = "">Whirlpool Microwave Oven Repair</a>&nbsp;&#124;
+<a href = "">Whirlpool AC Repair</a>&nbsp;&#124;
+<a href = "">Ifb Washing Machine Repair</a>&nbsp;&#124;
+<a href = "">Ifb Refrigerator Repair</a>&nbsp;&#124;
+<a href = "">Ifb Oven Repair</a>&nbsp;&#124;
+<a href = "">Ifb AC Repair</a>&nbsp;&#124;
+</div>
+<div class = "areas-div" id = "areas-div">
+<a href = "">Washing Machine Repair In Hyderabad</a>&nbsp;&#124;
+<a href = "">Refridgerator Repair In Hyderabad</a>&nbsp;&#124;
+<a href = "">Microwave Oven Repair In Hyderabad</a>&nbsp;&#124;
+<a href = "">Air Conditioner Repair In Hyderabad</a>&nbsp;&#124;
+</div>
+</div>
+</div>
+</section>
+
+<?php include_once("includes/modal-box.php"); ?>
+<?php include_once("includes/footer.php"); ?>
+</div>
+</body>
+</html>
